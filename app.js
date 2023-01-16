5909886ff25d36241884cdce9e5819d141b3ce6b
Yu-Che-Chang <a79967167@gmail.com>
2023-01-16 19:30:49 +0800

    feat: initialize project

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express')
+const port = 3000
+const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+require('./config/mongoose')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => console.log(`Now server is hosting on https://localhost:${port}`))
